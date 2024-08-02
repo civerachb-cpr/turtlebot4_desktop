1fc46dbec55f19b5eaf72ce0347df97a12b257a6
Chris Iverach-Brereton <civerachb@clearpathrobotics.com>
2024-08-02 12:18:56 -0400

    Separate model-view & navigation-view into separate launch files. Start updating the rviz parameters

--- turtlebot4_viz/launch/view_navigation.launch.py
+++ turtlebot4_viz/launch/view_navigation.launch.py
@@ -28,6 +28,10 @@ from launch_ros.actions import Node, PushRosNamespace
 
 ARGUMENTS = [
     DeclareLaunchArgument(
+        'use_sim_time',
+        default_value='false',
+        description='Use simulation (Gazebo) clock if true'),
+    DeclareLaunchArgument(
         'model',
         default_value='standard',
         choices=['standard', 'lite'],
@@ -47,7 +51,7 @@ def generate_launch_description():
     pkg_turtlebot4_description = get_package_share_directory('turtlebot4_description')
 
     rviz2_config = PathJoinSubstitution(
-        [pkg_turtlebot4_viz, 'rviz', 'model.rviz'])
+        [pkg_turtlebot4_viz, 'rviz', 'navigation.rviz'])
     description_launch = PathJoinSubstitution(
         [pkg_turtlebot4_description, 'launch', 'robot_description.launch.py']
     )
@@ -61,21 +65,12 @@ def generate_launch_description():
              executable='rviz2',
              name='rviz2',
              arguments=['-d', rviz2_config],
+             parameters=[{'use_sim_time': LaunchConfiguration('use_sim_time')}],
              remappings=[
                 ('/tf', 'tf'),
                 ('/tf_static', 'tf_static')
              ],
              output='screen'),
-
-        # Delay launch of robot description to allow Rviz2 to load first.
-        # Prevents visual bugs in the model.
-        TimerAction(
-            period=3.0,
-            actions=[
-                IncludeLaunchDescription(
-                    PythonLaunchDescriptionSource([description_launch]),
-                    launch_arguments=[('model', LaunchConfiguration('model'))]
-                )])
     ])
 
     ld = LaunchDescription(ARGUMENTS)
